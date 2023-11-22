1235ce7182395ee591bbffe115bd2853c798547e
Cesar Martel <cesar.martel9@gmail.com>
2023-11-22 13:52:56 +0100

    add chat context

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -46,7 +46,6 @@ const Dashboard = () => {
         <h1 className='mb-3 font-bold text-5xl text-gray-900'>
           My Files
         </h1>
-
         <UploadButton />
       </div>
 
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,29 +1,23 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import {
-  privateProcedure,
-  publicProcedure,
-  router,
-} from './trpc'
-import { TRPCError } from '@trpc/server'
-import { db } from '@/db'
-import { z } from 'zod'
-
-
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { privateProcedure, publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
+import { z } from "zod";
+import { INFINITE_QUERY_LIMIT } from "@/config/INFINITE_QUERY_LIMIT";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
-    const { getUser } = getKindeServerSession()
-    const user = getUser()
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
 
-    if (!user.id || !user.email)
-      throw new TRPCError({ code: 'UNAUTHORIZED' })
+    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
     // check if the user is in the database
     const dbUser = await db.user.findFirst({
       where: {
         id: user.id,
       },
-    })
+    });
 
     if (!dbUser) {
       // create user in db
@@ -32,43 +26,123 @@ export const appRouter = router({
           id: user.id,
           email: user.email,
         },
-      })
+      });
     }
 
-    return { success: true }
+    return { success: true };
   }),
   getUserFiles: privateProcedure.query(async ({ ctx }) => {
-    const { userId } = ctx
+    const { userId } = ctx;
 
     return await db.file.findMany({
       where: {
         userId,
       },
-    })
+    });
   }),
+
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const { fileId, cursor } = input;
+      const limit = input.limit ?? INFINITE_QUERY_LIMIT;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: fileId,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      const messages = await db.message.findMany({
+        take: limit + 1,
+        where: {
+          fileId
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+        select: {
+          id:true,
+          isUserMessage: true,
+          createdAt: true,
+          text: true
+        }
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined;
+      if(messages.length > limit) {
+        const nextItem = messages.pop();
+        nextCursor = nextItem?.id
+      }
+
+      return {
+        messages,
+        nextCursor,
+      }
+    }),
+
+  getFileUploadStatus: privateProcedure
+    .input(z.object({ fileId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const file = await db.file.findFirst({
+        where: {
+          id: input.fileId,
+          userId: ctx.userId,
+        },
+      });
+      if (!file) return { status: "PENDING" as const };
+      return { status: file.uploadStatus };
+    }),
+
+  getFile: privateProcedure
+    .input(z.object({ key: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      });
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return file;
+    }),
 
   deleteFile: privateProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      const { userId } = ctx
+      const { userId } = ctx;
 
       const file = await db.file.findFirst({
         where: {
           id: input.id,
           userId,
         },
-      })
+      });
 
-      if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
 
       await db.file.delete({
         where: {
           id: input.id,
         },
-      })
+      });
 
-      return file
+      return file;
     }),
-})
+});
 
-export type AppRouter = typeof appRouter
+export type AppRouter = typeof appRouter;
